b3173ca9a755c1e0ea0c790f14393543e0abeab8
appuser <appuser@fastmail.com>
2021-06-24 22:04:51 +1200

    started work creating cpp version of the app... no idea what I'm doing

---
+++ grocerrhea.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+class Ingredient {
+    private:
+        int id;
+        string name;
+        string filter;
+        bool required;
+    public:
+        Ingredient(int id, 
+                   string name, 
+                   string filter, 
+                   bool required = false) 
+        : id(id), name(name), filter(filter),
+          required(required) {}
+};
+
+class Recipe {
+    private:
+        int id;
+        string name;
+        string filter;
+        string* ingredients;
+        bool status;
+    public:
+        Recipe(int id,
+               string name,
+               string filter,
+               string* ingredients,
+               bool status = false)
+        : id(id), name(name), filter(filter), 
+          ingredients(ingredients), status(status) {}
+};
+
+
+string * getRequiredIngredients(string *items,
+                               string *recipes,
+                               string *ingredients,
+                               string *required_ingredients) {
+    // TODO Cull not needed recipe ingredients
+    // TODO Get new recipe ingredients
+    // TODO Cull ingredient items
+    // TODO Add ingredient items
+    // TODO return required ingredients
+}
+
+/* TODO Method Restore bool selection
+ * 
+ * bool_list
+ * flat_list
+ * 
+ */
+
+/* TODO Learn about Vectors and use those instead of multi-dimensional arrays
+ *
+ */
+
+/* TODO Method Make Grocery List
+ * 
+ * ingredients
+ * item choices
+ * restock ingredients
+ * 
+ */
+
+
+/* TODO Method Main/Session
+ * 
+ * ingredients
+ * recipes
+ * required_ingredients
+ * 
+ */
